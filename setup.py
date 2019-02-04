5afca1dd299aae138e9a9ba19fe05fc4c7b3818c
Sergey Kozlov <skozlovf@gmail.com>
2019-02-04 16:39:47 +0600

    Add license info

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
+# Copyright 2019 Luddite Labs Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import sys
 import os.path as op
 from setuptools import setup
@@ -33,8 +47,7 @@ setup(
     install_requires=['future>=0.16.0'],
     classifiers=[
         'Development Status :: 3 - Alpha',
-        # TODO: set license.
-        # 'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: Apache Software License',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
